60d2141b2daafdfff550cb5d941c865291321241
Abhay Rana <anabhay42@gmail.com>
2024-02-01 16:07:57 +0530

    add hindi option

--- Random-jokes-generator/script.js
+++ Random-jokes-generator/script.js
@@ -1,10 +1,14 @@
 let jokeArea = document.getElementById('joke-area')
-let nextBtn = document.getElementById('next-btn')
+let hindiBtn = document.getElementById('hindi-btn')
+let englishBtn = document.getElementById('english-btn')
+const apiKey = '661c5fa375b1339f89c1f185442f';
 
-function getJoke(){
+/*function for English Joke */
+function englishJoke(){
     fetch('https://v2.jokeapi.dev/joke/Any')
     .then(resolve=>resolve.json())
     .then(data=>{
+        console.log(data)
         if(data.joke){
             jokeArea.innerHTML = data.joke;
         }else{
@@ -12,8 +16,20 @@ function getJoke(){
         }
     })
 }
-
-nextBtn.addEventListener('click',()=>{
-    getJoke();
+/*function for Hindi Joke */
+function hindiJoke(){
+    let apiURL = `https://hindi-jokes-api.onrender.com/jokes?api_key=${apiKey}`;
+    fetch(apiURL)
+    .then(resolve=>resolve.json())
+    .then(data=>{
+        console.log(data)
+            jokeArea.innerHTML = data.jokeContent;
+    })
+}
+/* Event Handlers */
+englishBtn.addEventListener('click',()=>{
+    englishJoke();
 })
-getJoke();
+hindiBtn.addEventListener('click',()=>{
+    hindiJoke();
+})
